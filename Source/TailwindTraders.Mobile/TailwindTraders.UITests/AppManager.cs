fc52254f4662a013ed5c3b8209909cff939ce256
Sweeky Satpathy <swsat@microsoft.com>
2018-12-06 14:44:01 -0800

    Initial POP setup

---
+++ Source/TailwindTraders.Mobile/TailwindTraders.UITests/AppManager.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Xamarin.UITest;
+
+namespace TailwindTraders.UITests
+{
+    static class AppManager
+    {
+        const string ApkPath = "../../../Binaries/TaskyDroid.apk";
+        const string AppPath = "../../../Binaries/TaskyiOS.app";
+        const string IpaBundleId = "com.xamarin.samples.taskytouch";
+
+        static IApp app;
+        public static IApp App
+        {
+            get
+            {
+                if (app == null)
+                    throw new NullReferenceException("'AppManager.App' not set. Call 'AppManager.StartApp()' before trying to access it.");
+                return app;
+            }
+        }
+
+        static Platform? platform;
+        public static Platform Platform
+        {
+            get
+            {
+                if (platform == null)
+                    throw new NullReferenceException("'AppManager.Platform' not set.");
+                return platform.Value;
+            }
+
+            set
+            {
+                platform = value;
+            }
+        }
+
+        public static void StartApp()
+        {
+            if (Platform == Platform.Android)
+            {
+                app = ConfigureApp
+                    .Android
+                    // Used to run a .apk file:
+                    .ApkFile(ApkPath)
+                    .StartApp();
+            }
+
+            if (Platform == Platform.iOS)
+            {
+                app = ConfigureApp
+                    .iOS
+                    // Used to run a .app file on an ios simulator:
+                    .AppBundle(AppPath)
+                    // Used to run a .ipa file on a physical ios device:
+                    //.InstalledApp(ipaBundleId)
+                    .StartApp();
+            }
+        }
+    }
+}
---
+++ Source/TailwindTraders.Mobile/TailwindTraders.UITests/Tests/Tests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using NUnit.Framework;
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+
+namespace TailwindTraders.UITests
+{
+    public class Tests : BaseTestFixture
+    {
+        public Tests(Platform platform)
+            : base(platform)
+        {
+        }
+
+        [Test]
+        public void AppLaunches()
+        {
+            app.Repl();
+            app.Screenshot("First screen.");
+        }
+    }
+}
