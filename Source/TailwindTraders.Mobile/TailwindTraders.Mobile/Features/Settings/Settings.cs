d2610c2fc752b7b125fe933dab730a079d8b8db8
Marcos Cobeña Morián <marcoscm.digital@gmail.com>
2018-12-11 17:04:20 +0100

    Left changes

--- Source/TailwindTraders.Mobile/TailwindTraders.Mobile/Features/Settings/Settings.cs
+++ Source/TailwindTraders.Mobile/TailwindTraders.Mobile/Features/Settings/Settings.cs
@@ -1,29 +1,32 @@
-﻿namespace TailwindTraders.Mobile.Features.Settings
+namespace TailwindTraders.Mobile.Features.Settings
 {
     public static class Settings
     {
         public const string ApiAuthorizationHeader = "Authorization";
 
-        public static string AnonymousToken { get; } = "Email anonymous@anonymous.anonymous";
+        public const string AnonymousToken = "Email anonymous@anonymous.anonymous";
 
-        public static string RootApiUrl { get; } = "ENTER_YOUR_URL_HERE";
+        public const string RootApiUrl = "ENTER_YOUR_URL_HERE";
 
         public static string ProductApiUrl { get; } = $"{RootApiUrl}/products";
 
         public static string ProfilesApiUrl { get; } = $"{RootApiUrl}/profiles";
 
-        public static string AppCenterAndroidSecret { get; } = "ENTER_YOUR_GUID_HERE";
+        public const string AppCenterAndroidSecret = "ENTER_YOUR_GUID_HERE";
 
-        public static string AppCenteriOSSecret { get; } = "ENTER_YOUR_GUID_HERE";
+        public const string AppCenteriOSSecret = "ENTER_YOUR_GUID_HERE";
 
-        public static bool UseFakeAPIs { get; } =
-#if DEBUG
-            true;
-#else
-            false;
-#endif
+        public static bool UseFakeAPIs = DebugMode;
 
-        public static bool UseFakeAuthentication { get; } =
+        public static bool UseFakeAuthentication = DebugMode;
+
+        public static bool UseDebugLogging = DebugMode;
+
+        public static bool ForceAutomaticLogin = DebugMode;
+
+        public const bool AndroidDebuggable = DebugMode;
+
+        public const bool DebugMode =
 #if DEBUG
             true;
 #else
