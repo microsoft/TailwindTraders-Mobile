3c6e3fc630c00524e29f0523e70c5d47650856d8
Marcos Cobeña Morián <marcoscm.digital@gmail.com>
2018-12-12 13:49:29 +0100

    Revert "[Hopefully] fixed string replacement"
    This reverts commit a8c073fb505464d41ef348168c2dfd23a0245851.

--- Source/TailwindTraders.Mobile/TailwindTraders.Mobile.Android/appcenter-post-build.sh
+++ Source/TailwindTraders.Mobile/TailwindTraders.Mobile.Android/appcenter-post-build.sh
@@ -9,8 +9,11 @@
 
 PROJECTNAME="UnitTests"
 
+echo "Building projects matching \"${PROJECTNAME}\":"
+find $APPCENTER_SOURCE_DIRECTORY -regex '.*'"$PROJECTNAME"'\.csproj' -exec msbuild {} /p:Configuration=Release \;
+echo
 echo "Running tests matching \"${PROJECTNAME}\":"
-find $APPCENTER_SOURCE_DIRECTORY -regex '.*bin.*$PROJECTNAME\.dll' -exec nunit3-console {} \;
+find $APPCENTER_SOURCE_DIRECTORY -regex '.*bin.*'"$PROJECTNAME"'\.dll' -exec nunit3-console {} \;
 echo
 echo "Result:"
 pathOfTestResults=$(find $APPCENTER_SOURCE_DIRECTORY -name 'TestResult.xml')
