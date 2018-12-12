63de629e32fa9aeb221a2aa283a55ad9b866f723
Marcos Cobeña Morián <marcoscm.digital@gmail.com>
2018-12-12 15:25:46 +0100

    Fixed Xaml Styler back

--- Source/Tools/XamlStyler/run.sh
+++ Source/Tools/XamlStyler/run.sh
@@ -1,8 +1,10 @@
-if [[ ("$(uname)" == "Darwin" ) || ( "$(expr substr $(uname -s) 1 5)" == "Linux") ]];  then
+SOLUTIONNAME="TailwindTraders.Mobile"
+
+if [[ ("$(uname)" == "Darwin") || ("$(expr substr $(uname -s) 1 5)" == "Linux") ]]; then
     RUNTIME="mono"
 fi
 
-pushd ../../TailwindTraders.Mobile/
+pushd ../../$SOLUTIONNAME/
 
 $RUNTIME ../Tools/XamlStyler/XamlStyler.Console/xstyler.exe -c XamlStylerSettings.json -d . -r true
 
