a4bc87aa561b1b43b3ea7b8eb0f8d9a59afc4030
Marcos Cobeña Morián <marcoscm.digital@gmail.com>
2018-12-12 16:20:29 +0100

    Fixed wrong exchanged output

--- Source/TailwindTraders.Mobile/TailwindTraders.Mobile.iOS/appcenter-post-clone.sh
+++ Source/TailwindTraders.Mobile/TailwindTraders.Mobile.iOS/appcenter-post-clone.sh
@@ -4,15 +4,17 @@
 # XAML policies
 ########################################################################################################################
 
-echo
 echo "Verifying XAML policies:"
+echo
 cd $APPCENTER_SOURCE_DIRECTORY/Source/Tools/XamlStyler
 ./verify.sh
 
 if [[ $? -eq 0 ]]
 then 
+echo
+echo "Every XAML follows defined policies :-)" 
+else
+echo 
 echo "Not every XAML follows defined policies :-(" 
 exit 1
-else 
-echo "Every XAML follows defined policies :-)" 
 fi
