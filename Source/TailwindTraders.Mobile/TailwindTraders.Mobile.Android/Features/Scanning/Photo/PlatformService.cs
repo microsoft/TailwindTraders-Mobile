d5f0e08fb170c44e500e48574aa94a5ab9ad298b
Juan Antonio Cano <jacanosalado@gmail.com>
2018-12-17 16:30:49 +0100

    Build fix

--- Source/TailwindTraders.Mobile/TailwindTraders.Mobile.Android/Features/Scanning/Photo/PlatformService.cs
+++ Source/TailwindTraders.Mobile/TailwindTraders.Mobile.Android/Features/Scanning/Photo/PlatformService.cs
@@ -61,7 +61,9 @@ namespace TailwindTraders.Mobile.Droid.Features.Scanning.Photo
             var cleanPath = path.Replace("/", "_");
 
             // https://kimsereyblog.blogspot.com/2016/11/differences-between-internal-and.html
-            var absoluteFilePath = System.IO.Path.Combine(Android.App.Application.Context.FilesDir.AbsolutePath, cleanPath);
+            var absoluteFilePath = System.IO.Path.Combine(
+                Android.App.Application.Context.FilesDir.AbsolutePath,
+                cleanPath);
 
             var assets = Android.App.Application.Context.Assets;
             using (var f = assets.Open(path))
