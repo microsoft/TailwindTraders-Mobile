819cebcb8c710e595c0eaa822110b758f360691d
Marcos Cobeña Morián <marcoscm.digital@gmail.com>
2018-12-19 10:24:59 +0100

    Done tests

--- Source/TailwindTraders.Mobile/UnitTests/Features/Common/RestPoolServiceTests.cs
+++ Source/TailwindTraders.Mobile/UnitTests/Features/Common/RestPoolServiceTests.cs
@@ -1,34 +1,60 @@
 ﻿using NUnit.Framework;
 using TailwindTraders.Mobile.Features.Common;
 using TailwindTraders.Mobile.Features.Settings;
-using System;
 
 namespace UnitTests.Features.Common
 {
     public class RestPoolServiceTests
     {
+        private const string NewApiUrl = "foo";
+
+        private RestPoolService service;
+
+        public RestPoolServiceTests()
+        {
+            Settings.RootApiUrl = "http://foo.bar";
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            service = new RestPoolService();
+        }
+
         [Test]
         public void HomeApiIsRecreated()
         {
-            Settings.RootApiUrl = "http://github.com";
-
-            var service = new RestPoolService();
-
-            Assert.False(service.HomeAPI.IsValueCreated);
-
-            try
-            {
-                var useless = service.HomeAPI.Value;
-            }
-            catch (TypeLoadException)
-            {
-            }
+            var useless = service.HomeAPI.Value;
 
             Assert.True(service.HomeAPI.IsValueCreated);
 
-            service.UpdateApiUrl("foo");
+            service.UpdateApiUrl(NewApiUrl);
 
             Assert.False(service.HomeAPI.IsValueCreated);
+        }
+
+        [Test]
+        public void ProductsApiIsRecreated()
+        {
+            var useless = service.ProductsAPI.Value;
+
+            Assert.True(service.ProductsAPI.IsValueCreated);
+
+            service.UpdateApiUrl(NewApiUrl);
+
+            Assert.False(service.ProductsAPI.IsValueCreated);
+        }
+
+        [Test]
+        public void ProfilesApiIsRecreated()
+        {
+            var useless = service.ProfilesAPI.Value;
+
+            Assert.True(service.ProfilesAPI.IsValueCreated);
+
+            service.UpdateApiUrl(NewApiUrl);
+
+            Assert.False(service.ProfilesAPI.IsValueCreated);
         }
     }
 }
