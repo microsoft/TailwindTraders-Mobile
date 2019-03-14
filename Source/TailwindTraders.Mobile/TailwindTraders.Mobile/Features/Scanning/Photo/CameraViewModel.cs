ad2fc6d980d8e939dfb0622b2474804df755ea57
James Montemagno <james.montemagno@gmail.com>
2019-03-14 16:40:00 -0700

    Add to cart button

--- Source/TailwindTraders.Mobile/TailwindTraders.Mobile/Features/Scanning/Photo/CameraViewModel.cs
+++ Source/TailwindTraders.Mobile/TailwindTraders.Mobile/Features/Scanning/Photo/CameraViewModel.cs
@@ -1,7 +1,9 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Input;
 using TailwindTraders.Mobile.Features.Common;
+using TailwindTraders.Mobile.Features.Localization;
 using TailwindTraders.Mobile.Features.Product;
 using TailwindTraders.Mobile.Framework;
 using Xamarin.Forms;
@@ -20,7 +22,15 @@ namespace TailwindTraders.Mobile.Features.Scanning.Photo
 
         public ICommand CloseCommand => new AsyncCommand(App.NavigateModallyBackAsync);
 
-        public ICommand AddCommand => FeatureNotAvailableCommand;
+        protected async Task ShowAddToCartAsync()
+        {
+            await Application.Current.MainPage.DisplayAlert(
+                RecommendedProducts.First().Name,
+                Resources.Alert_Added_To_Cart,
+                Resources.Alert_OK);
+        }
+
+        public ICommand AddCommand => new AsyncCommand(ShowAddToCartAsync);
 
         public ICommand TakePhotoCommand => new AsyncCommand(App.NavigateModallyBackAsync);
 
